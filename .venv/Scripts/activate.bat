c1c240ab37167fbc3a7eb92953e3ff37dd5edc28
Bukola <queensdelight2011@gmail.com>
2024-06-03 22:43:50 -0500

    Your commit message

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\queen\Documents\datafun-06-eda\.venv
+set VIRTUAL_ENV=C:\Users\queen\OneDrive\Documents\datafun-06-eda\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
